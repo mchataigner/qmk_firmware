a67af0cc1d73f68da9ac8a01a9c687752e93c14d
Mathieu Chataigner <m.chataigner@criteo.com>
2016-01-13 23:58:17 +0100

    replace right tab with ralt

--- keyboard/ergodox_ez/keymaps/keymap_mat.c
+++ keyboard/ergodox_ez/keymaps/keymap_mat.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | App  | LGui |       | AltGr|Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
+ *                                 | Space|Backsp|------|       |------| AltGr  |Enter |
  *                                 |      |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_UP,  KC_DOWN,KC_LBRC,LT(MDIA, KC_RBRC),          KC_FN1,
              KC_RALT,        CTL_T(KC_ESC),
              KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+             KC_PGDN,KC_RALT, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
