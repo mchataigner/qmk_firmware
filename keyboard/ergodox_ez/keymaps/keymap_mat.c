9448aede43a505941d048caf1b5f380f039d7bf5
Mathieu Chataigner <m.chataigner@criteo.com>
2016-01-14 00:05:04 +0100

    replace left thumb backspace with lalt

--- keyboard/ergodox_ez/keymaps/keymap_mat.c
+++ keyboard/ergodox_ez/keymaps/keymap_mat.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | App  | LGui |       | AltGr|Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------| AltGr  |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 | Space| LAlt |------|       |------| AltGr  |Enter |
+ *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+                                               KC_SPC,KC_LALT,KC_END,
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_BSPC,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
